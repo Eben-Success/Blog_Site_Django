51bdcf9902ddbe6de96b8386a272ab4def368afc
Eben-Success <aebenezer237@gmail.com>
2022-10-18 23:52:09 +0000

    update

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>Blog Home </h1>')
-    
+
 def about(request):
-    return HttpResponse('<h1>Blog Home </h1>')
+    return HttpResponse('<h1>Blog About </h1>')
