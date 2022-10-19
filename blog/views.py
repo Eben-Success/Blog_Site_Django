ce21ea7ac4922d6db378d61464294e8637113a17
Eben-Success <aebenezer237@gmail.com>
2022-10-19 21:37:25 +0000

    update

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-{
-'author':'CoreyMS',
-'title': 'Blog Post 1',
-'content': 'First Post Content',
-'date_posted': 'August 27, 2022'
-},
-{
-    'author':'Success',
-    'title': 'Blog Post 2',
-    'content': 'Second Post Content',
-    'date_posted': 'August 29, 2022'
-},
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-    'posts': posts}
+    'posts': Post.objects.all()
+    }
     return render(request, 'blog/home.html', context)
 
 def about(request):
