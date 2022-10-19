c8eadb422d856ddad84a9a48a5650b5f0f0895f2
Eben-Success <aebenezer237@gmail.com>
2022-10-19 18:04:39 +0000

    update

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
