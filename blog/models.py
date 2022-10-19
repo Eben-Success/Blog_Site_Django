1593df954b1f5b87788d507954605e1d517e5276
Eben-Success <aebenezer237@gmail.com>
2022-10-19 18:09:24 +0000

    update

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,5 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
