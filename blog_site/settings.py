507899b6697092031a6fa8abc08cb3ce34981293
Eben-Success <aebenezer237@gmail.com>
2022-10-20 23:21:02 +0000

    update

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
-    "users.app.UsersConfig",
+    "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
