17a4b8353b05f390dccbfc627e5e5074c8280cc7
Eben-Success <aebenezer237@gmail.com>
2022-10-20 23:29:45 +0000

    update

--- users/views.py
+++ users/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
        else:
        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+#message.debug
+#message.success
+#message.info
+#message.warning
+#message.error
