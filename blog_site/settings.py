b0c63f61899d217b689626985cafc45fce694922
Eben-Success <aebenezer237@gmail.com>
2022-10-21 16:11:44 +0000

    update

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -118,6 +118,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 #Redirect to Homepage once loged In
 LOGIN_REDIRECT_URL = "blog-home"
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
