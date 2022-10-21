14673c25e0d3c3f0c6eb6572a9d4b744268de490
Eben-Success <aebenezer237@gmail.com>
2022-10-21 15:08:56 +0000

    update

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -21,8 +21,8 @@ from users import views as user_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.htlm'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('',include('blog.urls') ),
 
 ]
