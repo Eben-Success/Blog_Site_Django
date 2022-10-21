6b74e0ae5cbb866c58da1a23212a95a5bccf74b6
Eben-Success <aebenezer237@gmail.com>
2022-10-21 16:26:57 +0000

    update

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    # the profile must have one-to-one relationship with the logged in user
+    user = models.OneToOneField(User)
+    
