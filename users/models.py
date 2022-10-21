6150775bc6e6efa015cbe58150bf787a5e262161
Eben-Success <aebenezer237@gmail.com>
2022-10-21 16:28:05 +0000

    update

--- users/models.py
+++ users/models.py
@@ -4,5 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     # the profile must have one-to-one relationship with the logged in user
-    user = models.OneToOneField(User)
-    
+
+    # CASCADE: delete profile if user is delete.
+    # But leave user if profile is deleted.
+    user = models.OneToOneField(User, on_deete=models.CASCADE)
+
