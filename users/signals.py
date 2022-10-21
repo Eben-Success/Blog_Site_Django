73ab0fe4118cdd81be95f8879cd8f95f4207813c
Eben-Success <aebenezer237@gmail.com>
2022-10-21 18:51:12 +0000

    update

--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,9 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from .models import Profile
 
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
