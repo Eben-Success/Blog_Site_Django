d46e878295e7bcb06bc4b4cdcb2b313a53f9db15
Eben-Success <aebenezer237@gmail.com>
2022-10-23 13:02:40 +0000

    update

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>',PostDetailVie.as_view(), name='post-detail' )
+    path('post/<int:pk>',PostDetailView.as_view(), name='post-detail' ),
     path('about/', views.about, name='blog-about'),
 ]
 
