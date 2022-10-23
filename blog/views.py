74580a3190781e1b2e5d70b035b8d901b967716d
Eben-Success <aebenezer237@gmail.com>
2022-10-23 17:43:17 +0000

    update

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'
+    template_name = 'blog/home.html' #<app> / <model> _ <viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
