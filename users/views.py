88e8e91d7b09b6309131c4eb38be51b145d507dc
Eben-Success <aebenezer237@gmail.com>
2022-10-23 10:24:49 +0000

    update

--- users/views.py
+++ users/views.py
@@ -29,6 +29,11 @@ def profile(request):
         u_form = UserUpdateForm(request.POST, instance=request.user)
         #current profile
         p_form = ProfileUpdateForm(request.POST, request.FILES,  instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
