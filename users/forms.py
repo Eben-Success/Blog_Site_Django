62c406287bd12536f16bb6365d1124ba99ebed0b
Eben-Success <aebenezer237@gmail.com>
2022-10-23 10:02:08 +0000

    update

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
+from attr import field
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterFrom(UserCreationForm):
@@ -9,3 +11,18 @@ class UserRegisterFrom(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+# Create form that works with user model
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+# create a profile that gets updated
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        field = ['image']
