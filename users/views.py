39a3823dbc9f2d73f6df074401ac1d1c62a6d44c
Eben-Success <aebenezer237@gmail.com>
2022-10-23 10:26:30 +0000

    update

--- users/views.py
+++ users/views.py
@@ -33,6 +33,8 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
 
     else:
         u_form = UserUpdateForm(instance=request.user)
