d7bfef239581cbe96cb250c8339b7ccc9d81af68
Eben-Success <aebenezer237@gmail.com>
2022-10-23 10:58:22 +0000

    update

--- users/models.py
+++ users/models.py
@@ -21,6 +21,12 @@ class Profile(models.Model):
 
         img = Image.open(self.image.path)
 
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            #resize the image
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
 
 
 
