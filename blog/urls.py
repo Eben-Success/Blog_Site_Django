ccba38891f6fb9d0a4040ff9c4b136545e40f5ef
Eben-Success <aebenezer237@gmail.com>
2022-10-25 14:07:30 +0000

    update

--- blog/urls.py
+++ blog/urls.py
@@ -11,7 +11,7 @@ from .views import(
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/',PostDetailView.as_view(), name='post-detail' ),
-    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('about/', views.about, name='blog-about'),
 ]
