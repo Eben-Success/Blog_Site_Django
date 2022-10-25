305b55146818d355fa98ea8a35bc6b731ebaf243
Eben-Success <aebenezer237@gmail.com>
2022-10-25 14:03:55 +0000

    update

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ UpdateView
 )
 from django.shortcuts import render
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 # Create your views here.
@@ -44,13 +44,19 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
         
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
-    def form_valide(self, form):
+    def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
         
 
     
