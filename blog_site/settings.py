cddfc2d9b8db79fa7580d3e712c2f5d6aa3648fc
Eben-Success <aebenezer237@gmail.com>
2022-10-29 23:00:14 +0000

    update

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -123,6 +123,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
