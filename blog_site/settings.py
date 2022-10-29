6c59973e0015888146856baf835fe7c1191cead5
Eben-Success <aebenezer237@gmail.com>
2022-10-29 23:02:20 +0000

    update

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -124,6 +124,11 @@ LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
