7ea9dd4a1be3f1519f08b7e942da8a1c1e0bac4a
Eben-Success <aebenezer237@gmail.com>
2022-10-30 07:35:26 +0000

    update

--- users/models.py
+++ users/models.py
@@ -16,8 +16,8 @@ class Profile(models.Model):
         return f'{self.user.username} profile'
 
     # override save method to resize image
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
